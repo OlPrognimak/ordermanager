eb6fad4e73a4ed053c3529f8fb8a0228d8753a69
Oleksandr Prognimak <oleksandr.prognimak@crowdcode.io>
2020-09-16 16:31:14 +0200

    Fix mapping person to to personForm. Add getting path to reports from system environment. Add disclaimer to classes.

--- ordermanager-backend/src/main/java/com/pr/ordermanager/controller/JasperReportController.java
+++ ordermanager-backend/src/main/java/com/pr/ordermanager/controller/JasperReportController.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2020, Oleksandr Prognimak. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oleksandr Prognimak
+ *     may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.pr.ordermanager.controller;
 
 import com.pr.ordermanager.service.JasperReportService;
@@ -9,6 +39,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * The rest controller for generation pdf report
+ * @author Oleksandr Prognimak
+ */
 @RestController
 @CrossOrigin
 public class JasperReportController {
@@ -17,14 +51,11 @@ public class JasperReportController {
     @Autowired
     JasperReportService jasperReportService;
 
-
-
-    public void initController(){
-
-    }
     /**
-     *
-     * @param invoiceNumber
+     * Produce pdf report from report data in database for report which is
+     * defined by {@code invoiceNumber}
+     * @param invoiceNumber the number of invoice
+     * @return  the response with pdf report
      */
     @GetMapping("/invoice/report/{invoiceNumber}")
     @ResponseBody
--- ordermanager-backend/src/main/java/com/pr/ordermanager/service/EntityToModelMapperHelper.java
+++ ordermanager-backend/src/main/java/com/pr/ordermanager/service/EntityToModelMapperHelper.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2020, Oleksandr Prognimak. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oleksandr Prognimak
+ *     may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.pr.ordermanager.service;
 
 import com.pr.ordermanager.controller.model.*;
@@ -9,6 +39,8 @@ import java.util.stream.Collectors;
 
 /**
  * The mapping helper for mapping JPA Entity to Rest Model POJO object
+ *
+ * @author Oleksandr Prognimak
  */
 public class EntityToModelMapperHelper {
 
@@ -95,7 +127,7 @@ public class EntityToModelMapperHelper {
 
     public static List<DropdownDataType> mapPersonToDropdownType(List<Person> persons){
         return persons.stream().map(p->new DropdownDataType(
-                Utils.emptyOrValue(p.getPersonLastName())+ " "+
+                Utils.emptyOrValue(p.getPersonFirstName())+ " "+
                         Utils.emptyOrValue(p.getPersonLastName())+ " "+
                         Utils.emptyOrValue(p.getCompanyName()).trim(),
                 ""+p.getId())).collect(Collectors.toList());
