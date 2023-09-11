349d68742cd6178ec5905acc31422ee3ec70f709
Oleksandr Prognimak <ol.prognimak@gmail.com>
2023-09-11 18:18:49 +0200

    Initial commit. Create initial html template and component script.

---
+++ ordermanager-ui/ui/resources/frontend/invoices/src/app/invoice/item-management/item-management.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SharedModule} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {ToastModule} from "primeng/toast";
+import {InvoiceItemModel, ItemCatalogModel} from "../../domain/domain.invoiceformmodel";
+import {InvoicePipesModule} from "../../common-services/common-services.pipes.number";
+import {ButtonModule} from "primeng/button";
+
+@Component({
+  selector: 'app-item-management',
+  standalone: true,
+  imports: [CommonModule, SharedModule, TableModule, ToastModule, InvoicePipesModule, ButtonModule],
+  templateUrl: './item-management.component.html',
+  styleUrls: ['./item-management.component.css']
+})
+export class ItemManagementComponent {
+  invoiceItems: ItemCatalogModel[]
+
+  isInvoiceChanged(invoiceitem: ItemCatalogModel) {
+
+  }
+
+  rowDoubleClick($event: MouseEvent, invoiceitem: ItemCatalogModel) {
+
+  }
+
+  showDeleteItemDialog(id: number) {
+    
+  }
+}
