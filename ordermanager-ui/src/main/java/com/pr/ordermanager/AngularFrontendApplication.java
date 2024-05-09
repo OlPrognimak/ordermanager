0079a1595fa21b5931d4bb849076994b0297cf07
Oleksandr Prognimak <oleksandr.prognimak-ldi@wescaleit.com>
2024-05-09 22:44:52 +0200

    Enable support java version 21 creates two profiles to fix angular build on Linux or Windows.

--- ordermanager-ui/src/main/java/com/pr/ordermanager/AngularFrontendApplication.java
+++ ordermanager-ui/src/main/java/com/pr/ordermanager/AngularFrontendApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Stringboot application for frontend
